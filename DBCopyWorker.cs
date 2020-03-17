cc7e4b9471eb9d75591ae8d8f4eba976049aafe8
jpliu <vincywindy@live.cn>
2020-03-17 11:43:32 +0800

    修复错误

--- DBCopyWorker.cs
+++ DBCopyWorker.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Z.BulkOperations;
+using Z.EntityFramework.Extensions;
 
 namespace EntityFrameworkCore
 {
@@ -21,26 +22,39 @@ namespace EntityFrameworkCore
     /// </summary>
     public class DBCopyWorker<T> where T : DbContext
     {
-        readonly T _fromDb;
-        readonly T _toDb;
+        readonly DbContextOptions<T> _fromDbConfig;
+        readonly DbContextOptions<T> _toDbConfig;
         readonly DBCopyConfig _config;
         readonly List<IEntityType> _entityTypes;
         readonly Type _listtype;
         readonly MethodInfo _bulkinsert = typeof(DbContextExtensions).GetMethods().Where(d => d.Name == "BulkInsert" && d.GetParameters().Length == 3).First();
+        readonly Action<BulkOperation> act = new Action<BulkOperation>((d) => { d.InsertKeepIdentity = true; });
         /// <summary>
         /// Inti CopyWorker
         /// </summary>
         /// <param name="FromDb">Which Db is Copy From</param>
         /// <param name="ToDb">Which Db is Copy To</param>
         /// <param name="config">The config of copy db</param>
-        public DBCopyWorker(T FromDb, T ToDb, DBCopyConfig config = null)
+        public DBCopyWorker(DbContextOptions<T> FromDb, DbContextOptions<T> ToDb, DBCopyConfig config = null)
         {
+            if (EntityFrameworkManager.ContextFactory == null)
+            {
+                EntityFrameworkManager.ContextFactory = context =>
+                {
+                    return (T)Activator.CreateInstance(typeof(T), ToDb);
+                };
+            }
+
             _listtype = typeof(List<>);
-            _fromDb = FromDb;
-            _toDb = ToDb;
+            _fromDbConfig = FromDb;
+            _toDbConfig = ToDb;
             _config = config;
-            _entityTypes = FromDb.Model.GetEntityTypes().Where(d => !d.IsAbstract()).ToList();
-            CheckShadow(_entityTypes);
+            using (var context = (T)Activator.CreateInstance(typeof(T), FromDb))
+            {
+                _entityTypes = context.Model.GetEntityTypes().Where(d => !d.IsAbstract()).ToList();
+                CheckShadow(_entityTypes);
+            }
+
 
         }
         /// <summary>
@@ -59,7 +73,7 @@ namespace EntityFrameworkCore
         {
             Export();
             Import();
-        
+
         }
         /// <summary>
         /// Import the data to "To" Context
@@ -67,52 +81,113 @@ namespace EntityFrameworkCore
         /// <returns></returns>
         public void Import()
         {
-            var Finishdic = Path.Combine(_config.WorkPath, "Finish");
-            if (!Directory.Exists(Finishdic))
-            {
-                Directory.CreateDirectory(Finishdic);
-            }
-            foreach (var t in _entityTypes)
-            {
-                var tablename = t.GetTableName();
-                var dbname = t.DisplayName();
-                var jsonpath = Path.Combine(_config.WorkPath, dbname + ".json");
-                if (!File.Exists(jsonpath))
-                {
-                    Info($"{jsonpath} is no exists,skip to import");
-                }
-                var dbInserttype = _listtype.MakeGenericType(t.ClrType);//list<T>
 
-                var entitylist = JsonHelper.GetJson(jsonpath, dbInserttype);
-                var list = new List<object>();
-                foreach (var en in entitylist)
+            using (var _toDb = (T)Activator.CreateInstance(typeof(T), _toDbConfig))
+            {
+                var Finishdic = Path.Combine(_config.WorkPath, "Finish");
+                if (!Directory.Exists(Finishdic))
                 {
-                    list.Add(en);
-                    InsertCount++;
-                    if (list.Count >= _config.ProcessCount)
+                    Directory.CreateDirectory(Finishdic);
+                }
+                foreach (var t in _entityTypes)
+                {
+                    var tablename = t.GetTableName();
+                    var dbname = t.DisplayName();
+                    var jsonpath = Path.Combine(_config.WorkPath, dbname + ".json");
+                    if (!File.Exists(jsonpath))
                     {
-                        BulkInsert(t, tablename, dbInserttype, list);
+                        Info($"{jsonpath} is no exists,skip to import");
+                        continue;
                     }
+                    var dbInserttype = _listtype.MakeGenericType(t.ClrType);//list<T>
+
+                    var entitylist = JsonHelper.GetJson(jsonpath, dbInserttype);
+                    IList list = (IList)Activator.CreateInstance(dbInserttype);
+                    foreach (var en in entitylist)
+                    {
+                        list.Add(en);
+                        InsertCount++;
+                        if (list.Count >= _config.ProcessCount)
+                        {
+                            BulkInsert(_toDb, t, tablename, dbInserttype, list);
+                            list.Clear();
+                            Info($"Inserted {tablename} count:{InsertCount}");
+                        }
+
+                    }
+                    if (list.Count > 0)
+                    {
+                        BulkInsert(_toDb, t, tablename, dbInserttype, list);
+                        list.Clear();
+                        Info($"Inserted {tablename} count:{InsertCount}");
+                    }
+                    var finjsonpath = Path.Combine(Finishdic, dbname + ".json");
+                    if (File.Exists(finjsonpath))
+                    {
+                        File.Delete(finjsonpath);
+                    }
+                    File.Move(jsonpath, finjsonpath);
+                    Info($"Inserted {tablename} Count:{InsertCount}");
+
 
                 }
-                var finjsonpath = Path.Combine(Finishdic, dbname + ".json");
-                if (File.Exists(finjsonpath))
-                {
-                    File.Delete(finjsonpath);
-                }
-                File.Move(jsonpath, finjsonpath);
-                Info($"Inserted{tablename}数据");
-
-
             }
         }
+        private void GetQuestioninfo(T _toDb, IEntityType t, Type dbInserttype, IList list)
+        {
+            Info($"Check Error list {list.Count}");
 
-        public virtual void BulkInsert(IEntityType t, string tablename, Type dbInserttype, List<object> list)
+            if (list.Count == 1)
+            {
+                Error(JsonConvert.SerializeObject(list[0]));
+            }
+            var halflist = (IList)Activator.CreateInstance(dbInserttype);
+            foreach (var ls in list)
+            {
+
+                halflist.Add(ls);
+                if (halflist.Count >= list.Count / 2)
+                {
+                    try
+                    {
+                        _bulkinsert.MakeGenericMethod(t.ClrType).Invoke(_toDb, new object[] { _toDb, halflist, act });
+                        halflist.Clear();
+                    }
+                    catch
+                    {
+                        GetQuestioninfo(_toDb, t, dbInserttype, halflist);
+                    }
+                }
+            }
+            if (halflist.Count > 0)
+            {
+                try
+                {
+                    _bulkinsert.MakeGenericMethod(t.ClrType).Invoke(_toDb, new object[] { _toDb, halflist, act });
+                    halflist.Clear();
+                }
+                catch
+                {
+                    GetQuestioninfo(_toDb, t, dbInserttype, halflist);
+                }
+            }
+
+        }
+        public virtual void BulkInsert(T _toDb, IEntityType t, string tablename, Type dbInserttype, IList list)
         {
             var listcopy = (IList)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(list), dbInserttype);
 
-            var act = new Action<BulkOperation>((d) => { d.InsertKeepIdentity = true; });
-            _bulkinsert.MakeGenericMethod(t.ClrType).Invoke(_toDb, new object[] { _toDb, list, act });
+
+            try
+            {
+                _bulkinsert.MakeGenericMethod(t.ClrType).Invoke(_toDb, new object[] { _toDb, list, act });
+         
+            }
+            catch (Exception ex)
+            {
+                GetQuestioninfo(_toDb, t, dbInserttype, list);
+                throw ex;
+            }
             var updatelist = Check(list, listcopy, dbInserttype);
             if (updatelist.Count > 0)
             {
@@ -147,48 +222,55 @@ namespace EntityFrameworkCore
             }
             return list;
         }
-     
+
         /// <summary>
         /// Export the data from "From" Context
         /// </summary>
         /// <returns></returns>
         public void Export()
         {
-
-            foreach (var t in _entityTypes)
+            using (var _fromDb = (T)Activator.CreateInstance(typeof(T), _fromDbConfig))
             {
-                var tablename = t.GetTableName();
-                var dbname = t.DisplayName();
-                var jsonpath = Path.Combine(_config.WorkPath, dbname + ".json");
-                if (File.Exists(jsonpath))
+                foreach (var t in _entityTypes)
                 {
-                    //already had,skip
-                    Info($"{jsonpath} already existed,skip to load");
-                    continue;
-                }
-                var entitylist = new List<object>(_config.ProcessCount);
-                var count = 0;
-                using (var ls = _fromDb.Set(t.ClrType).AsNoTracking().GetEnumerator())
-                {
-
-                    Info($"Get data from {dbname}");
-                    while (ls.MoveNext())
+                    var tablename = t.GetTableName();
+                    var dbname = t.DisplayName();
+                    var jsonpath = Path.Combine(_config.WorkPath, dbname + ".json");
+                    if (File.Exists(jsonpath))
                     {
-                        entitylist.Add(ls.Current);
+                        //already had,skip
+                        Info($"{jsonpath} already existed,skip to load");
+                        continue;
+                    }
+                    var entitylist = new List<object>(_config.ProcessCount);
+                    var count = 0;
+                    using (var ls = _fromDb.Set(t.ClrType).AsNoTracking().GetEnumerator())
+                    {
 
-                        LoadCount++;
-                        count++;
-                        if (entitylist.Count >= _config.ProcessCount)
+                        Info($"Get data from {dbname}");
+                        while (ls.MoveNext())
+                        {
+                            entitylist.Add(ls.Current);
+
+                            LoadCount++;
+                            count++;
+                            if (entitylist.Count >= _config.ProcessCount)
+                            {
+                                JsonHelper.AddJson(jsonpath, entitylist);
+                                entitylist.Clear();
+                            }
+
+                        }
+                        if (entitylist.Count > 0)
                         {
                             JsonHelper.AddJson(jsonpath, entitylist);
                             entitylist.Clear();
                         }
 
                     }
+                    Info($"Loaded {tablename},total {count},save to {jsonpath}");
 
                 }
-                Info($"Loaded {tablename},total {count},save to {jsonpath}");
-
             }
         }
         private void CheckShadow(List<IEntityType> Entitys)
